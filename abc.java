3ff1c171e47c70a4d07c465848d6f1c31bb723d4
ridimaborase-cyber <ridimaborase@gmail.com>
2025-09-28 18:51:20 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,11 @@
+public class abc
+{
+public static void main(String args[])
+{
+Integer obj = new Integer(100);
+int num = obj.intValue();
+//Creating Wrapper class object
+//Converting the wrapper object to primitive
+System.out.println(num+ " <--> "+ obj);
+}
+}
